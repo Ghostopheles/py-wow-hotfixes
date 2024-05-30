4c0983c8af416a927ca51f36c9284b662e982d34
Ghostopheles <ghost@kivatech.io>
2024-05-29 19:14:03 -0500

    Run options

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import os
+import time
+import hashlib
+
+from client.automation import ClientAutomation
 from hotfixes.parser import HotfixParser, Flavor
 
 FLAVOR = Flavor.Live
@@ -6,4 +11,39 @@ GAME_PATH = "F:/Games/World of Warcraft"
 DBCACHE_PATH = f"{GAME_PATH}/{FLAVOR}/Cache/ADB/enUS/DBCache.bin"
 
 parser = HotfixParser(GAME_PATH, FLAVOR)
-parser.print_hotfixes()
+
+
+def get_file_hash(file_path: str):
+    hash_func = hashlib.new("sha256")
+
+    with open(file_path, "rb") as f:
+        while chunk := f.read(8192):
+            hash_func.update(chunk)
+
+    return hash_func.hexdigest()
+
+
+client = ClientAutomation(os.path.join(GAME_PATH, FLAVOR))
+
+
+def run_loop():
+    loop_time = 60  # seconds
+    last_hash = None
+    while True:
+        client.launch()
+
+        new_hash = get_file_hash(DBCACHE_PATH)
+        if new_hash != last_hash:
+            parser.print_hotfixes()
+
+        last_hash = new_hash
+        time.sleep(loop_time)
+
+
+def run_once():
+    client.launch()
+    parser.print_hotfixes()
+
+
+if __name__ == "__main__":
+    run_once()
